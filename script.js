0ef634dba24884eed15618027c5e1d252d27b1b7
SkipidiDev <245475087+SkipidiDev@users.noreply.github.com>
2026-03-20 21:32:08 +0700

    Add function to generate random computer choice

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 console.log('Hello, World!');
 
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"]; // array starting with 0 index
+  const randomIndex = Math.floor(Math.random() * choices.length); // length is 3, so it generates a number between 0 and 2.
+  return choices[randomIndex];
+}
+
+console.log(getComputerChoice()); // this will return either "rock", "paper", or "scissors" randomly.
